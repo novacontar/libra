dd2b7e8e94cce3084614aa219b808c9c51bc219f
main <50725386+novacontar@users.noreply.github.com>
2025-03-07 14:09:06 +0100

    added logger

--- contar_mod.py
+++ contar_mod.py
@@ -2,6 +2,24 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils.vis_utils import plot_model
 
+# # configure logging
+# import torch.nn as nn
+# import torch
+# import sentencepiece as spm
+# from datasets import load_dataset
+# import math
+# import os
+
+import logging as log
+
+outfile = "simple_transformer.log"
+log.basicConfig(
+    level=log.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[log.FileHandler(outfile), log.StreamHandler()],
+    force=True,  # Resets
+)
 
 
 def nn_print_plot(model, out_file_name="model_plot.png"):
@@ -22,8 +40,6 @@ def nn_model_test(number_outputs_1, number_outputs_2):
     :return: keras nn model
     """
     model = Sequential()
-    model.add(Dense(number_outputs_1, input_dim=5, activation='relu'))
-    model.add(Dense(number_outputs_2, activation='sigmoid'))
+    model.add(Dense(number_outputs_1, input_dim=5, activation="relu"))
+    model.add(Dense(number_outputs_2, activation="sigmoid"))
     return model
-
-
